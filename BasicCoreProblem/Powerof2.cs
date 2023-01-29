8a93a229296361c0f6709a4ee56a3471000635ce
“Vssandip” <sandipshindesr95035@gmail.com>
2023-01-29 11:39:41 +0530

    [ SANDIP ] Power of 2

---
+++ BasicCoreProblem/Powerof2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Powerof2
+    {
+       public static void powerof2()
+        {
+            int n;
+
+            Console.WriteLine("Give the Exponent");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            double value = Math.Pow(2, n);
+
+            Console.WriteLine("Result is : " + value);
+        }
+
+    }
+}
