bcd61fc9d236806a22c9a44f5df37fc460496f1a
“Vssandip” <sandipshindesr95035@gmail.com>
2023-01-29 12:12:54 +0530

    [ SANDIP ] Even Or Odd Number

---
+++ BasicCoreProblem/Number is Even or Odd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Number_is_Even_or_Odd
+    {
+       public static void number_is_Even_or_Odd()
+        {
+            Console.WriteLine("Enter a Number: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+
+            {
+
+                Console.WriteLine(n + " is a Even Number.");
+
+            }
+            else
+
+            {
+
+                Console.WriteLine(n + " is a Odd Number.");
+
+            }
+        }
+    }
+}
