d3c8cd00704420b63cdab16eb0fd3507755bf250
“Vssandip” <sandipshindesr95035@gmail.com>
2023-01-29 12:08:54 +0530

    [ SANDIP ] Swap Two Number

---
+++ BasicCoreProblem/Swap Two Numbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Swap_Two_Numbers
+    {
+       public static void swap_Two_Numbers()
+        {
+            int a, b, temp;
+
+            Console.WriteLine("Enter first number: ");
+
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+
+            b = int.Parse(Console.ReadLine());
+
+            temp = a;
+
+            a = b;
+
+            b = temp;
+
+            Console.WriteLine("After swapping {0},{1}", a, b);
+        }
+    }
+}
