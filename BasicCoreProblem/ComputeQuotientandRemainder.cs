2242d10353bb6a429564da2431b6d5e223a5e663
“Vssandip” <sandipshindesr95035@gmail.com>
2023-01-29 12:03:43 +0530

    [ SANDIP ] Remainder

---
+++ BasicCoreProblem/ComputeQuotientandRemainder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class ComputeQuotientandRemainder
+    {
+       public static void computeQuotientandRemainder()
+        {
+
+            int Dividend = 22, Divisor = 2;
+
+            int Quotient = Dividend / Divisor;
+
+            int Remainder = Dividend % Divisor;
+
+            Console.WriteLine("Dividend: {0} Divisor:{1}", Dividend, Divisor);
+
+            Console.WriteLine("Qotient = " + Quotient);
+
+            Console.WriteLine("Remainder = " + Remainder);
+        }
+    }
+}
